ad5a4fd8392d9c8e37ea5b23a9da6be8981d1070
Nathan Budiman <jonathan@radialogica.com>
2020-05-28 14:42:00 -0500

    Random number generator complete
    Could add more features

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,7 +2,25 @@
 
 import random
 
-def main():
-    input("Press enter to generate new password")
+def randGen():
+    #length
+    length = random.randint(10, 20)
+    pw = ""
 
+    for x in range(length):
+        c = random.randint(1,2)
+        if c == 1:
+            a = random.randint(0,9)
+            a = str(a)
+        else:
+            a = random.randint(97, 122)
+            a = chr(a)
+            a = str(a)
+        pw+=a
+    return pw
+
+
+def main():
+    input("Press enter to generate a new random password")
+    print(randGen())
 main()
