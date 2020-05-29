22179728f26dcf489a027ccc1138fa962c6e5916
Nathan Budiman <jonathan@radialogica.com>
2020-05-29 08:39:34 -0500

    Keyword retrieval
    Keyword retrieval made, integration not yet designed

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -21,7 +21,32 @@ def randGen(letterPref, length):
         pw+=a
     return pw
 
+    #WIP
+def keyWord(keyWordList, pw):     #Method that adds in keywords at random points within the random method
+    while (True):
+        start = random.randint(1, len(pw))
 
+
+
+def chooseKeyWord(length):
+    count = 0
+    if length == -1:
+        length = 20
+    addWord = input("Would you like to add a key word to be included in your random password?\n1. Yes\n2. No\n")
+    print("Keep in mind adding a keyword may mess with your number/letter ratio preference")
+    if addWord == "1":
+        while (True):
+            keyWordstr = input("Enter the words you would like to have included in your password, add a space between each word and do not exeed the length limit\n")
+            keyWordList = keyWordstr.split()
+            for x in keyWordList:
+                count += len(x)
+            if count <= length:
+                break
+            print("Your chosen keywords are longer than the given password length, choose less keywords")
+            count = 0
+        return keyWordstr.split()
+    return -1
+        
 def main():
     print("Your password system has been updated; We can now handle prefernces toward random passwords")
     letterPref = input("On a scale of 1-10 (1 being all numbers, 10 being all letters), choose your lettertype prefernce.\n")
@@ -38,7 +63,15 @@ def main():
     else:
         #-1 indicates that length will be random
         length = -1
-    
+
+    keyWordList = chooseKeyWord(length)
+
     input("Press enter to generate a new random password")
-    print(randGen(letterPref, length))
+    pw = randGen(letterPref, length)
+
+    #System adds keywords after generating random password
+    if keyWordList != -1:
+        keyWord(keyWordList, pw)
+
+    print(pw)
 main()
